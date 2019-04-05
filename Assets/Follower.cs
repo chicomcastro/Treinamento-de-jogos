358e9988ffa394fc64ade197a42dfeff03f60f9a
Francisco Castro <Franciscommcastro@gmail.com>
2019-04-05 13:45:05 -0300

    Update and comment follower.cs

--- Assets/Follower.cs
+++ Assets/Follower.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Script for applying when we desire a follow mechanic on Y axis (for now)
 public class Follower : MonoBehaviour
 {
     public Transform target;
@@ -14,13 +15,14 @@ public class Follower : MonoBehaviour
 
     void Update()
     {
+        // Wait for initial conditions / setup
         if (Mathf.Abs(transform.position.y - target.position.y) < tolerance && !haveMoved)
         {
             offset = transform.position.y - target.position.y;
             t0 = Time.time;
             return;
         }
-        else
+        else // Move effectivally following target
         {
             haveMoved = true;
             float t = Time.time - t0;
@@ -29,6 +31,7 @@ public class Follower : MonoBehaviour
             offset += tolerance * (1 - Mathf.Pow((2.71f), -w * t));
         }
 
+        // Set desiredPosition to pursue, respecting constrains of Z axis (only for 2D games)
         Vector3 desiredPosition = target.position;
         desiredPosition.y += offset;
         desiredPosition.z = transform.position.z;
