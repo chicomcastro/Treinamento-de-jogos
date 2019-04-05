5a514a77b5135bc1ca91cb2152edf843a08679e7
Francisco Castro <Franciscommcastro@gmail.com>
2019-04-05 13:52:07 -0300

    Update and commenting MotorController.cs
    For rocket scene

--- Assets/MotorController.cs
+++ Assets/MotorController.cs
@@ -4,33 +4,44 @@ using UnityEngine;
 
 public class MotorController : MonoBehaviour
 {
+    // Public atributes
+    // Declare dinamic and static parameters
     public float rocketMass = 0.5f;
     public float propellerMass = 0.5f;
     public float propellerForce = 10.0f;
-
-    private Rigidbody2D rb;
-    // Start is called before the first frame update
+    public float massFlux = 0.1f;
+    
+    // Private atributes
+    private Rigidbody2D rb;     // Reference to Rigidbody2D component attached to this object
+    
     void Start()
     {
+        // Get reference to correct component privatly
         rb = GetComponent<Rigidbody2D>();
+        
+        // Check if it goes without any errors
         if (rb == null)
             Debug.LogWarning("There's no rigidbody attached to " + gameObject.name);
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
+        // Used for autolaunch and control
         // rb.velocity = new Vector3(0,10,0);
         // return;
-
+        
+        // Interaction with player
         if (Input.GetButton("Vertical"))
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, propellerForce));
+            // Add a propulsion forse to rigidbody2D (rb)
+            rb.AddForce(new Vector2(0, propellerForce));
+            
+            // Reduces mass of propellent with use
             if (GetComponent<Rigidbody2D>().mass > rocketMass)
             {
-                propellerMass -= (float)0.1 * Time.deltaTime;
+                propellerMass -= (float) massFlux * Time.deltaTime;
             }
-            GetComponent<Rigidbody2D>().mass = rocketMass + propellerMass;
+            rb.mass = rocketMass + propellerMass;
         }
     }
 }
