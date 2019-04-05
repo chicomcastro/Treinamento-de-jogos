9b0fa3ef452c1a4587452cb9c2c4673788b5beea
Francisco Castro <Franciscommcastro@gmail.com>
2019-04-05 14:31:13 -0300

    Update and comment PlayerMovement.cs
    - Rename private auxilliar variables
    - Propose exercices about this script to practice

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,48 +4,56 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    public Animator anim;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+    // Public atributes
+    public float speed;         // Setting speed for our player
+    public Animator anim;       // We're gonna use public reference for animator (manually attached from Editor)
+    
     void Update()
     {
-        Vector2 newPos;
-        newPos = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
+        // Perform basic inputs to know in which direction player should move
+        Vector2 deltaPos;
+        deltaPos = new Vector2(0, 0);       // Initially we don't know where to move, só \delta x = (0,0)
+        if (Input.GetKey(KeyCode.W))        // Get input data from W key and set respectivelly direction 
         {
-            newPos += new Vector2(0, 1);
+            deltaPos += new Vector2(0, 1);
             anim.Play("player_up");
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))   // Get input data from A key and set respectivelly direction 
         {
-            newPos += new Vector2(-1, 0);
+            deltaPos += new Vector2(-1, 0);
             anim.Play("player_left");
         }
-        else if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))   // Get input data from S key and set respectivelly direction 
         {
-            newPos += new Vector2(0, -1);
+            deltaPos += new Vector2(0, -1);
             anim.Play("player_down");
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))   // Get input data from D key and set respectivelly direction 
         {
-            newPos += new Vector2(1, 0);
+            deltaPos += new Vector2(1, 0);
             anim.Play("player_right");
         }
 
-        if (newPos.magnitude == 0)
-            anim.SetBool("haveStoped", true);
+        // Used for animate our states in animator controller
+        if (deltaPos.magnitude == 0)            // If we're not gonna move this frame
+            anim.SetBool("haveStoped", true);   // Set one of idle states animation
         else
-            anim.SetBool("haveStoped", false);
+            anim.SetBool("haveStoped", false);  // Else, we're moving then do not wanna animation transitions
 
-        //anim.SetFloat("vel_y", newPos.y);
-        //anim.GetFloat("vel_x");
+        // This is some examples for access animator parameter data (remember to create them and access with the correct name)
+        //anim.SetFloat("vel_y", newPos.y);     // Set is linked to write new values, then we have two arguments
+        //anim.GetFloat("vel_x");               // Get is linked to read values from existing atributes
 
-        transform.position += (Vector3)newPos.normalized * speed * Time.deltaTime;
+        // For finish, we have to tell our transform for where we're want to move based on speed magnitude and at a
+        // fixed amount of time (remember that Time.deltaTime is very important to normalize time between frame calls)
+        transform.position += (Vector3)deltaPos.normalized * speed * Time.deltaTime;
     }
 }
+
+/* EXERCISES:
+1) Try to move using method Input.GetButton(string) instead of Input.GetKey(KeyCode)
+2) Try to get values from Input.GetAxis(string) to define deltaPos direction
+3) Implement public KeyCodes (as global atributes) to define player controls publically from Editor
+4) Use public KeyCodes implemented on exercise 3 to implement a second player (make other animated object with the same script, but
+with other KeyCodes to control movement.
+*/
